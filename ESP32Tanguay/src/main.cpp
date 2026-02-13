dfe4e7e8cd7420732b5a7351975a24319fae741f
EBhero <etiennebrisson@hotmail.com>
2026-02-13 13:32:22 -0500

    ab le to set color

--- ESP32Tanguay/src/main.cpp
+++ ESP32Tanguay/src/main.cpp
@@ -2,18 +2,21 @@
 #include <FastLED.h>
 #include <WiFi.h>
 #include "StripMethods.cpp"
-#include <WebServer.h>
+
+#include <ESPAsyncWebServer.h>
 
 const char *ssid = "Hornblende";
 const char *password = "minuscule";
 
-WebServer server(80);
+const char* PARAM_MESSAGE = "message";
 
-void handleRoot() {
-  String json = "{\"response:\" : \"Hello world\"}";
-  
-  server.send(200, "application/json", json);
+AsyncWebServer server(80);
+
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
 }
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -36,16 +39,48 @@ void setup() {
   Serial.println(WiFi.localIP());
   SetOnboardLED(CRGB::Green);
 
-  server.on("/", handleRoot);
-  
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(200, "text/plain", "Hello, world");
+  });
+
+  // Send a GET request to <IP>/get?message=<message>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+      String message;
+      if (request->hasParam(PARAM_MESSAGE)) {
+          message = request->getParam(PARAM_MESSAGE)->value();
+      } else {
+          message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, GET: " + message);
+  });
+
+  // Send a POST request to <IP>/post with a form field message set to <message>
+  server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+      String message;
+      if (request->hasParam(PARAM_MESSAGE, true)) {
+          message = request->getParam(PARAM_MESSAGE, true)->value();
+          if (message == "Red")
+            SetOnboardLED(CRGB::Red);
+          if (message == "Blue")
+            SetOnboardLED(CRGB::Blue);
+          if (message == "Green")
+            SetOnboardLED(CRGB::Green);
+      } else {
+          message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, POST: " + message);
+  });
+
+  server.onNotFound(notFound);
 
   server.begin();
-  Serial.println("HTTP server started");
 }
 
 
 
 void loop() {
-    server.handleClient();
 }
 
