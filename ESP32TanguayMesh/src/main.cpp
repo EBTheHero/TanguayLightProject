bad2f9fdfecf4492c003631432b2f973a855fa34
EBhero <etiennebrisson@hotmail.com>
2026-04-02 03:32:13 -0400

    Meshed and LED together

--- ESP32TanguayMesh/src/main.cpp
+++ ESP32TanguayMesh/src/main.cpp
@@ -19,87 +19,182 @@ bool playPattern = false;
 bool onboardLEDOn = true;
 #define ONBOARD_LED 2
 
-void receivedCallback(uint32_t from, String &msg)
-{
+#define REDPIN   4
+#define GREENPIN 19
+#define BLUEPIN  23
 
-	if (msg == "o")
-		digitalWrite(ONBOARD_LED, HIGH);
-	if (msg == "f")
-		digitalWrite(ONBOARD_LED, LOW);
+#define LED_ACTIVE true
 
-	Serial.println("Received from " + String(from) + ": " + msg.c_str());
-}
+#define Brightness 0.5
+
+bool connected = false;
 
 void newConnectionCallback(uint32_t nodeId)
 {
 	Serial.printf("New connection, nodeId = %u", nodeId);
 }
 
+
+
+
+void BeginStrips()
+{
+    pinMode(REDPIN,   OUTPUT);
+    pinMode(GREENPIN, OUTPUT);
+    pinMode(BLUEPIN,  OUTPUT);
+}
+
+// This function takes the incoming RGB values and outputs the values
+// on three analog PWM output pins to the r, g, and b values respectively.
+void showAnalogRGB( const CRGB& rgb)
+{
+  analogWrite(REDPIN,   rgb.r * Brightness);
+  analogWrite(GREENPIN, rgb.g * Brightness);
+  analogWrite(BLUEPIN,  rgb.b * Brightness);
+}
+
+void receivedCallback(uint32_t from, String &msg)
+{
+
+	if (msg == "r") {
+		showAnalogRGB(CRGB::Red);
+	}
+	else if (msg == "g") {
+		showAnalogRGB(CRGB::Green);
+	}
+	else if (msg == "b") {
+		showAnalogRGB(CRGB::Blue);
+	}
+	else if (msg == "w") {
+		showAnalogRGB(CRGB::White);
+	}
+}
+
+CRGB Wheel(byte WheelPosition) {
+  static byte c[3];
+ 
+  if(WheelPosition < 85) {
+   c[0] = WheelPosition * 3;
+   c[1] = 255 - WheelPosition * 3;
+   c[2] = 0;
+  }
+  else if(WheelPosition < 170) {
+   WheelPosition -= 85;
+   c[0] = 255 - WheelPosition * 3;
+   c[1] = 0;
+   c[2] = WheelPosition * 3;
+  }
+  else {
+   WheelPosition -= 170;
+   c[0] = 0;
+   c[1] = WheelPosition * 3;
+   c[2] = 255 - WheelPosition * 3;
+  }
+
+  
+  return CRGB(c[0], c[1], c[2]);
+}
+
+void Rainbow() {
+
+	CRGB hue = Wheel(millis() / 10);
+    // Use FastLED automatic HSV->RGB conversion
+    showAnalogRGB(hue);
+}
+
+void PulseWhite() {
+
+	int x = (millis() / 2) % 510;
+	if (x <= 255) {
+		showAnalogRGB(CRGB(x, x, x));
+	}
+	else {
+		showAnalogRGB(CRGB(255 - x, 255 - x, 255 - x));
+	}
+
+	delay(20);
+}
+
 void setup()
 {
-	BeginStrips();
-	SetBrightness(40);
-	SetAllLED(CRGB::Yellow);
+	if (LED_ACTIVE) {
+		BeginStrips();
+	}
 
 	// put your setup code here, to run once:
 	Serial.begin(115200);
 	delay(50);
 	Serial.println("Hello world");
 
-
-
-
 	pinMode(ONBOARD_LED, OUTPUT);
 
 
-	if (Serial)
-		mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION);
-	// mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
-	// mesh.onReceive(&receivedCallback);
-	// mesh.onNewConnection(&newConnectionCallback);
 
 
-	SetAllLED(CRGB::White);
+	mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
+	mesh.onReceive(&receivedCallback);
+	mesh.onNewConnection(&newConnectionCallback);
+
+	if (Serial) {
+		mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION | MESH_STATUS);
+
+	}
+
+
+
+	showAnalogRGB(CRGB::White);
+	delay(500);
+	showAnalogRGB(CRGB::Black);
+
+	// Serial.println("White");
 	//Serial.println("Setup complete!");
 }
 
 void loop()
 {
-	Rainbow();
+	// Rainbow();
+	// delay(20);
 
-	// mesh.update();
-	// userScheduler.execute();
+	mesh.update();
+	userScheduler.execute();
 
-	// if (millis() % 1000 == 0) {
+	// Fade white when not inside mesh AKA alone
+	if (mesh.getNodeList().size() == 0) {
+		PulseWhite();
+		connected = false;
+	} else if (!connected) {
+		showAnalogRGB(CRGB::Green);
+		delay(200);
+		showAnalogRGB(CRGB::Black);
 
-	// 	list<uint32_t> nodeList = mesh.getNodeList();
+		connected = true;
+	}
 
-	// 	int size = nodeList.size();
 
-	// 	if (Serial)
-	// 		Serial.println("Mesh Size: " + size);
-	// }
+	if (millis() % 1000 == 0) {
 
-	// if (Serial.available())
-	// {
-	// 	Serial.println(Serial.available());
+		
+	}
 
-	// 	char c = Serial.read();
+	if (Serial.available())
+	{
+		// Serial.println(Serial.available());
 
-	// 	if (c == 'o')
-	// 	{
-	// 		mesh.sendBroadcast("o", true);
-	// 	}
-	// 	if (c == 'f')
-	// 	{
-	// 		mesh.sendBroadcast("f", true);
-	// 	}
-	// }
+		char c = Serial.read();
 
-	// int thousands = millis() % 1000;
+		if (c == 'j')
+			Serial.println(mesh.subConnectionJson());
+		else
+			mesh.sendBroadcast(String(c), true);
+		// if (c == 'r')
+		// 	mesh.sendBroadcast("r", true);
+		// else if (c == 'g')
+		// 	mesh.sendBroadcast("g", true);
+		// else if (c == 'b')
+		// 	mesh.sendBroadcast("b", true);
+		// else if (c == 'w')
+		// 	mesh.sendBroadcast("w", true);
 
-	// if (thousands <= 500)
-	// 	digitalWrite(ONBOARD_LED, HIGH);
-	// if (thousands >= 500)
-	// 	digitalWrite(ONBOARD_LED, LOW);
+	}
+
 }
--- PythonServerBridge/main.py
+++ PythonServerBridge/main.py
@@ -1,15 +1,45 @@
 import time
 import serial
+import tkinter as tk
 
 ser = serial.Serial(
     port='COM5',
     baudrate=115200
 )
 
-while True:
-    ser.write(bytes('o', "utf-8"))
-    print("LED ON")
-    time.sleep(0.5)
-    ser.write(bytes('f', "utf-8"))
-    print("LED OFF")
-    time.sleep(0.5)
+def green_click():
+    ser.write(b"g")
+
+def red_click():
+    ser.write(b"r")
+
+def blue_click():
+    ser.write(b"b")
+
+def layout_click():
+    ser.write(b"j")
+
+def update_clock():
+    # Execute your logic here
+    while ser.in_waiting > 0:
+        # Read a line and decode from bytes to string
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+
+    root.after(1000, update_clock) # Schedule itself again in 1 second
+    
+
+root = tk.Tk()
+root.title("Send green")
+button = tk.Button(root, text="green", command=green_click)
+button.pack(pady=20)
+button2 = tk.Button(root, text="red", command=red_click)
+button2.pack(pady=5)
+button3 = tk.Button(root, text="blue", command=blue_click)
+button3.pack(pady=5)
+button4 = tk.Button(root, text="mesh layout", command=layout_click)
+button4.pack(pady=5)
+update_clock()
+root.mainloop()
+
+
